446e6f80f1f2230c6d4ccebe2afba2f7c7a9b1e5
Weii Wang <weii.wang@canonical.com>
2026-01-23 19:05:28 +0800

    Add network.py

---
+++ src/network.py
@@ -0,0 +1,27 @@
+# Copyright 2026 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Network related functions."""
+
+import json
+import subprocess
+
+
+def get_router_id() -> str:
+    """Get router ID of this machine.
+
+    Return:
+        Router ID as string.
+    """
+    out = subprocess.check_output(["ip", "-4", "-j", "route", "get", "1.2.3.4"], encoding="utf-8")  # noqa: S607
+    return json.loads(out)[0]["prefsrc"]
+
+
+def get_network_interface() -> str:
+    """Get main network interface.
+
+    Return:
+        Network interface name.
+    """
+    out = subprocess.check_output(["ip", "-4", "-j", "route", "get", "1.2.3.4"], encoding="utf-8")  # noqa: S607
+    return json.loads(out)[0]["dev"]
