266024fd2545a10ab1d5a331b40d94656dbaf714
Weii Wang <weii.wang@canonical.com>
2026-01-26 13:59:15 +0800

    Apply suggestions from review comments

--- src/bird.py
+++ src/bird.py
@@ -80,7 +80,7 @@ def bird_reload(config: str) -> None:
     """
     if _BIRD_CONF_FILE.read_text(encoding="utf-8") != config:
         _BIRD_CONF_FILE.write_text(config, encoding="utf-8")
-    subprocess.check_call(["birdc", "configure"])  # noqa: S607
+        subprocess.check_call(["birdc", "configure"])  # noqa: S607
 
 
 def bird_apply_db(
